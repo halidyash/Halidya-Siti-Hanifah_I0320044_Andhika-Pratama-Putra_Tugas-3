79aa4131cca2fe25baac5c1d5c05e8fd7e817873
Halidya Siti Hanifah <halidyahani@gmail.com>
2021-03-19 20:57:37 +0700

    penugasan soal 2

--- I0320044_Soal 2_Tugas 3.py
+++ I0320044_Soal 2_Tugas 3.py
@@ -1,18 +1,22 @@
 #Dictionary
 
-dict = {'Nama':'Halidya Siti Hanifah',
-        'Hobi':('Bermain musik','Design','Menonton film'),
-        'Sosial media':('Instagram:@halidyash','Line : halidyaash','Whatsapp : 087884754527'),
-        'Lagu kesukaan':('To The Bone - Pamungkas','ILYSB - LANY','Positions - Ariana Grande','Lowkey - NIKI'),
-        'Makanan Favorit':('Cheesecake','Lasagna','Batagor')}
+dict = {'Nama':'Halidya Siti Hanifah', 'Hobi 1':'Bermain musik', 'Hobi 2':'Design', 'Hobi 3':'Menonton film',
+        'Sosial media 1':'Instagram @halidyash','Sosial media 2':'Line : halidyaash','Sosial media 3':'Whatsapp : 087884754527',
+        'Lagu kesukaan 1':'To The Bone - Pamungkas','Lagu kesukaan 2':'ILYSB - LANY','Lagu kesukaan 3':'Positions - Ariana Grande','Lagu kesukaan 4':'Little Things - 1D',
+        'Makanan Favorit 1':'Cheesecake','Makanan Favorit 2':'Lasagna','Makanan Favorit 3':'Batagor'}
 print(dict)
 print("")
 
-dict['Hobi'] = ('Bermain musik','Masak','Menonton film')
-dict['Sosial media'] = ('Instagram : @halidyash','Twitter : @halidyash','Whatsapp : 087884754527')
+#Mengubah hobi dan sosial media
+dict['Hobi 2'] = ('Memasak')
+dict['Sosial media 2'] = ('Twitter : @halidyash6')
 
-dict['Makanan Favorit'] = ('Cheesecake')
+#menghapus dua makanan favorit
+del dict['Makanan Favorit 1']
+del dict['Makanan Favorit 2']
 
-dict['Hobi'] = ('Bermain musik','Masak','Menonton film','Mendengarkan lagu')
+#menambah satu hobi
+dict['Hobi 4'] = ('Mendengarkan lagu')
 
+#mencetak hasil
 print(dict)
